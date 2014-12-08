7c07ec69129ffaa26d2bf5410249b5402aff7dd3
olinares <olinares9833@gmail.com>
2014-12-08 12:56:51 -0800

    Added product alert messages, update,delete function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,29 @@ class ProductsController < ApplicationController
     @product = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
 
   def create
-    @product = Product.new(product_params)
-    if @product.save
-      redirect_to products_path
-    else
-      render :new
+    if @product = Product.create(product_params)
+      flash[:notice] = "Product was created successfully"
+      redirect_to products_path(@product)
     end
   end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product was updated successfully"
+      redirect_to products_path
+    end
+  end
+
 
   def edit
     @product = Product.find(params[:id])
@@ -24,10 +35,11 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     if @product.destroy
+      flash[:notice] = "Product was deleted"
       redirect_to products_path
     end
   end
-  
+
   private
 
   def product_params
--- db/migrate/20141208201344_create_products.rb
+++ db/migrate/20141208201344_create_products.rb
@@ -4,6 +4,8 @@ class CreateProducts < ActiveRecord::Migration
       t.string :name
       t.text :description
       t.integer :price
+      t.datetime :created_at
+      t.datetime :updated_at
     end
   end
 end
